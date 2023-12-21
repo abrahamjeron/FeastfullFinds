6fc57de229b156744e0198bfbc56599c6a192452
abrahamjeron <abrahamjeron40@gmail.com>
2023-12-21 09:50:20 +0530

    changes done

--- scripts/recipie.js
+++ scripts/recipie.js
@@ -1,4 +1,41 @@
-// This is the main JS file where the functionalities of the main page(hoome page) is written
+// This page is to show the details of the Random recipe which is selected by the user
+
+// This will get the unique id of the Random recipe selected by the users which is fetched from the local storage just to avoid re-rendering
+const randomRecipeid = localStorage.getItem("random")
+fetch (`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${randomRecipeid}`)
+    .then(response=>{
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json()
+    })
+    .then(data =>{
+        const ingredientContents = document.getElementById("ingredients")
+        const Ingredients = [];
+        for (let i = 1; i <= 20; i++) {
+          const ingredient = data.meals[0]['strIngredient'+i];
+          if (ingredient && ingredient.trim() !== "") {
+            Ingredients.push(ingredient);
+          } else {
+            break;
+          }
+        }
+        // details of the selected recipe will be shown by this following codes
+        ingredientContents.innerText=Ingredients
+        const instruction = document.getElementById("instruction")
+        instruction.innerText=data.meals[0].strInstructions
+        const recipeImg = document.querySelector(".recipie-img")
+        var imageUrl = data.meals[0].strMealThumb
+        recipeImg.innerHTML=`<img id="recipie-img" src=${imageUrl} alt="">`
+        var videoLink = document.getElementById("link-container")
+        videoLink.innerHTML=`<a href=${data.meals[0].strYoutube}>Watch Video</a>`
+        // <a href="">Watch Video</a>
+        console.log(data.meals[0].strYoutube)
+    })
+    .catch(error=>{
+        console.error('Fetch error:', error);
+    })
+
 
 // If the search button is clicked, then the following functionalities will come into play
 // Giving event listener to the search button
@@ -47,65 +84,15 @@ function fetchCategory(){
           // If the try it button on the recipe element is clicked, then the following function will happen
           tryIt.addEventListener('click',function(){
             console.log("clicked",meal.idMeal)
-            // fetching the meal id from the object
             var mealId = meal.idMeal
-            // Storing the meal id to the local storage
             localStorage.setItem('mealid',mealId)
-            location.href="./recipiepag2.html"
+            location.href="./recipiepage.html"
           })
       });
       // If the search button is clicked, the page will redirect to this location in the webpage
-      location.href="index.html#list"
+      
   })
   // Manipulating the errors
   .catch(error => console.error('Error fetching data:', error));
   }
 
-
-  // This is to fetch the random meal from the API
-fetch ("https://www.themealdb.com/api/json/v1/1/random.php")
-    .then(response=>{
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json()
-    })
-    .then(data =>{
-        // console.log(data);
-        const recipieName = document.getElementById("recipe-name")
-        // The ingredients will be in this array
-        const Ingredients = [];
-        for (let i = 1; i <= 20; i++) {
-          const ingredient = data.meals[0]['strIngredient'+i];
-          if (ingredient && ingredient.trim() !== "") {
-            Ingredients.push(ingredient);
-          } else {
-            break;
-          }
-        }
-        console.log(Ingredients);
-        var imgSrc=data.meals[0].strMealThumb
-        recipieName.innerText=data.meals[0].strMeal
-        // the image of the random recipie come here
-        const randomRecipie = document.getElementById("recipie-img")
-        randomRecipie.innerHTML=`<img id="recipie-img1" src="${data.meals[0].strMealThumb}" alt="">`
-        localStorage.setItem('random',data.meals[0].idMeal)
-    })
-    // Manipulating the errors
-    .catch(error=>{
-        console.error('Fetch error:', error);
-    })
-
-// this try button is for the random recipie.
-const TryBtn = document.getElementById("try-btn")
-TryBtn.addEventListener("click",function(){
-  setTimeout(() => {
-    location.href="./recipiepage.html"
-  }, 300);
-})
-// if the trybutton is clicked , then the fork image will be toggled to the animation 
-const tryBtn = document.getElementById("try-btn")
-const fork = document.getElementById("fork")
-tryBtn.addEventListener("click",function(){
-    fork.style.animation='fork 0.5s linear'
-})
